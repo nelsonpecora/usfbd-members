9e673c46a7f949b37d0025342b38088103bf8142
Nelson Pecora <nelson@keats.me>
2023-01-10 21:55:13 -0500

    updated login
    - less restrictive lastName matching
    - better error messages

--- src/data/hashes.js
+++ src/data/hashes.js
@@ -4,7 +4,7 @@ const members = require('./members')();
 module.exports = () => {
   return members.reduce((acc, member) => {
     const id = member.id;
-    const lastName = member.lastName.toLowerCase().trim();
+    const lastName = member.lastName.toLowerCase().replaceAll(/[^a-z]/g, '');
 
     acc[id] = hash(`${id}${lastName}`);
     return acc;
--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -40,13 +40,28 @@ function login (e) {
 
   const data = new window.FormData(document.querySelector('#login-form'));
   const id = data.get('id');
-  const lastName = data.get('name').toLowerCase().trim();
+  const lastName = data.get('name').toLowerCase().replaceAll(/[^a-z]/g, '');
   const hashed = hash(`${id}${lastName}`);
+  const attempts = document.querySelector('.login-attempts');
+  const error1 = document.querySelector('.error1');
+  const error2 = document.querySelector('.error2');
+
+  attempts.value++;
+  console.log(attempts.value)
 
   // Check to see if this is a real member
   if (!window.hashes[id] || window.hashes[id] !== hashed) {
-    window.alert('Member not found! Please double check what you entered');
+    error1.classList.add('show');
+
+    if (attempts.value > 1) {
+      const link = error2.querySelector('a');
+
+      link.href = link.href.replace('$1', id).replace('$2', lastName);
+      error2.classList.add('show');
+    }
   } else {
+    error1.classList.remove('show');
+    error2.classList.remove('show');
     window.sessionStorage.setItem('auth', hashed);
     // Redirect member to their page
     window.location.href = `/${id}.html`;
