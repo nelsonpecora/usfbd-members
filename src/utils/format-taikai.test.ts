1519ec55be229e62405af029be5431da6a507f9a
Nelson Pecora <hi@nelson.codes>
2026-04-12 23:24:11 -0400

    fix format and test checks

--- src/utils/format-taikai.test.ts
+++ src/utils/format-taikai.test.ts
@@ -7,32 +7,30 @@ describe("formatTaikaiWin", () => {
 
   it("formats 1st place", () => {
     expect(formatTaikaiWin([{ place: 1, name: "Spring Taikai" }])).toBe(
-      "<strong>1st Place:</strong> Spring Taikai"
+      "<strong>1st Place:</strong> Spring Taikai",
     );
   });
 
   it("formats 2nd place", () => {
     expect(formatTaikaiWin([{ place: 2, name: "Fall Taikai" }])).toBe(
-      "<strong>2nd Place:</strong> Fall Taikai"
+      "<strong>2nd Place:</strong> Fall Taikai",
     );
   });
 
   it("formats 3rd place", () => {
     expect(formatTaikaiWin([{ place: 3, name: "National Taikai" }])).toBe(
-      "<strong>3rd Place:</strong> National Taikai"
+      "<strong>3rd Place:</strong> National Taikai",
     );
   });
 
   it("formats other place as 'Won'", () => {
     expect(formatTaikaiWin([{ place: 4, name: "Regional Taikai" }])).toBe(
-      "<strong>Won:</strong> Regional Taikai"
+      "<strong>Won:</strong> Regional Taikai",
     );
   });
 
   it("formats win with no place as 'Won'", () => {
-    expect(formatTaikaiWin([{ name: "Open Taikai" }])).toBe(
-      "<strong>Won:</strong> Open Taikai"
-    );
+    expect(formatTaikaiWin([{ name: "Open Taikai" }])).toBe("<strong>Won:</strong> Open Taikai");
   });
 
   it("joins multiple wins with <br />", () => {
@@ -41,9 +39,9 @@ describe("formatTaikaiWin", () => {
         { place: 1, name: "Spring Taikai" },
         { place: 2, name: "Fall Taikai" },
         { name: "Open Taikai" },
-      ])
+      ]),
     ).toBe(
-      "<strong>1st Place:</strong> Spring Taikai<br /><strong>2nd Place:</strong> Fall Taikai<br /><strong>Won:</strong> Open Taikai"
+      "<strong>1st Place:</strong> Spring Taikai<br /><strong>2nd Place:</strong> Fall Taikai<br /><strong>Won:</strong> Open Taikai",
     );
   });
 });
